7be38429e99af215ea971b97b701fe1c7140b574
zhaomingqiang <zhaomingqiang@bytedance.com>
2022-06-28 15:41:34 +0800

    增加 code error

---
+++ error/code_err.go
@@ -0,0 +1,48 @@
+package error
+
+import (
+	"errors"
+	"fmt"
+)
+
+type CodeError struct {
+	code int
+	msg  string
+	err  error
+}
+
+func NewCodeErrorOfMessage(code int, msg string) *CodeError {
+	return NewCodeErrorWarp(code, msg, nil)
+}
+
+func NewCodeErrorOfMessagef(code int, format string, args ...interface{}) *CodeError {
+	msg := fmt.Sprintf(format, args...)
+	return NewCodeErrorWarp(code, msg, nil)
+}
+
+func NewCodeError(code int, err error) *CodeError {
+	return NewCodeErrorWarp(code, err.Error(), err)
+}
+
+func NewCodeErrorWarp(code int, msg string, err error) *CodeError {
+	return &CodeError{code: code, msg: msg, err: err}
+}
+
+func (ce *CodeError) Code() int {
+	return ce.code
+}
+
+func (ce *CodeError) Msg() string {
+	return ce.msg
+}
+
+func (ce *CodeError) Err() error {
+	if ce.err == nil {
+		return errors.New(ce.msg)
+	}
+	return ce.err
+}
+
+func (ce *CodeError) Error() string {
+	return fmt.Sprintf("code:%d, msg:%s, err:%v", ce.code, ce.msg, ce.err)
+}
